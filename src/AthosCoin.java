f4d3c36471a757459cd7a63f5a349cd98ab8d6ac
Dunet Tom <tom18dunet@gmail.com>
2023-04-11 15:07:37 +0200

    supp de chose

--- src/AthosCoin.java
+++
@@ -1,5 +0,0 @@
-public class AthosCoin extends Cryptomonnaie{
-    public AthosCoin(){
-        super("ATC", 8815788);
-    }
-}
--- src/PorthosCoin.java
+++
@@ -1,5 +0,0 @@
-public class PorthosCoin extends Cryptomonnaie{
-    public PorthosCoin(){
-        super("PTC", 666);
-    }
-}
